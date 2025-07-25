f2cd52b74342723dd0ef6c4989fb9a6d42317c8e
Raihan Ahmed <raihanxupdima@gmail.com>
2025-07-25 21:43:05 +0600

    Improving time complexity and performance.

--- Easy/3487LeetCode.cpp
+++ Easy/3487LeetCode.cpp
@@ -1,52 +1,33 @@
 // Problem: https://leetcode.com/problems/maximum-unique-subarray-sum-after-deletion
 // Category: Easy
 // Language: C++
-// Time Complexity: O(n), Space Complexity: O(n)
+// Time Complexity: O(n), Space Complexity: O(1)
 
 
 class Solution {
 public:
     int maxSum(vector<int>& nums) {
-        unordered_set<int>numsTwo;
-        vector<int>nT;
-        int k=0;
+        unordered_set<int> seen;
+        int sum=0, minusSum=INT_MIN;
+        bool positive = false;
 
-        while(k<nums.size()){
-            if(numsTwo.count(nums[k])){
-                k++;
-                continue;
-            }else{
-                if(nums[k]<0){
-                    k++;
-                    continue;
+        for(auto val: nums){
+            if(!seen.count(val)){
+                if(val>0){
+                    positive=true;
+
+                    seen.insert(val);
+                    sum+=val;
+                }else if(!positive){
+                    minusSum=max(minusSum, val);
                 }
-                numsTwo.insert(nums[k]);
-                nT.push_back(nums[k++]);
             }
         }
 
-        unordered_set<int> s;
-        int maxS=INT_MIN, temp=0;
-        int st=0;
-
-        if(nT.size()==0){
-            for(auto val: nums){
-                maxS=max(maxS, val);
-            }
+        if(!positive){
+            sum=minusSum;
         }
 
-        for(int en=0; en<nT.size(); en++){
-            while(s.find(nT[en])!=s.end()){
-                temp-=nT[st];
-                s.erase(nT[st++]);
-            }
-
-            s.insert(nT[en]);
-            temp+=nT[en];
-
-            maxS=max(maxS, temp);
-        }
-
-        return maxS;
+        return sum;
     }
 };
